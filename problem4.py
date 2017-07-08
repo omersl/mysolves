f8f14a720177ecbcbb23542f4202befbd770179e
omersl <selomer@yandex.com>
2017-07-08 07:19:54 +0300

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -1,8 +1,8 @@
-def is_palindrome(number):
+def pythonpali(number):
     return str(number) == str(number)[::-1]
 
 if __name__ == "__main__":
     print('{} and {} => {}'.format(*max((i,j,i*j)
                                         for i in range(1000, 900, -1)
                                         for j in range(1000, 900, -1)
-                                        if is_palindrome(i*j))))
+                                        if pythonpali(i*j))))
