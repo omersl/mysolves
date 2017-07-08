c5846ba851e84ae9c59a14fbb5683d84bfccb1da
omersl <selomer@yandex.com>
2017-07-08 07:18:38 +0300

    Create problem4.py

---
+++ problem4.py
@@ -0,0 +1,8 @@
+def is_palindrome(number):
+    return str(number) == str(number)[::-1]
+
+if __name__ == "__main__":
+    print('{} and {} => {}'.format(*max((i,j,i*j)
+                                        for i in range(1000, 900, -1)
+                                        for j in range(1000, 900, -1)
+                                        if is_palindrome(i*j))))
